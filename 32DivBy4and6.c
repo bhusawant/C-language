23fcf4420aa32e9dd04341a14ba593f59b1dfecc
Bhuvan <102148710+bhusawant@users.noreply.github.com>
2023-03-16 21:39:17 +0530

    Add files via upload

---
+++ 32DivBy4and6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i%4==0 && i%6==0)
+        {
+            printf("%d ", i);
+        }
+    }
+}
